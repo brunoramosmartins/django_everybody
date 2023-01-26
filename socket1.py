04e92d2f584afc6f4985878303daa7cf6f8c7427
brunoramosmartins <brunoramosmartins1989@gmail.com>
2023-01-25 22:50:04 -0300

    Building a Simple HTTP Server in Python.

--- socket1.py
+++ socket1.py
@@ -2,7 +2,7 @@ import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80)) # domínio e porta
-cmd = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
+cmd = 'GET http://data.pr4e.org/page2.htm HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
 while True:
